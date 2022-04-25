95238b49fc4d91571d73c0361c8edae11d5c5bbc
Rachel Bailey <rachel.ebailey@yahoo.com>
2022-04-25 18:53:55 -0400

    added braids information array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
---
+++ src/shared/braids.js
@@ -0,0 +1,68 @@
+export const BRAIDS = [
+    {
+        id: 0,
+        name: 'Anastasia',
+        price: 45,
+        image: 'assets/anastasia.jpg',
+        threestrand: true,
+        fivestrand: true
+    },
+    {
+        id: 1,
+        name: 'Five Strand Swirl',
+        price: 35,
+        image: 'assets/five-strand-swirl.jpg',
+        threestrand: false,
+        fivestrand: true
+    },
+    {
+        id: 2,
+        name: 'Five Strand Classic',
+        price: 45,
+        image: 'assets/five-strand-classic.PNG',
+        threestrand: false,
+        fivestrand: true
+    },
+    {
+        id: 3,
+        name: 'Funky Panda',
+        price: 50,
+        image: 'assets/funky-panda.JPG',
+        threestrand: true,
+        fivestrand: true
+    },
+    {
+        id: 4,
+        name: 'Genie',
+        price: 76,
+        image: 'assets/genie.jpg',
+        threestrand: true,
+        fivestrand: false
+    },
+    {
+        id: 5,
+        name: 'Lace Crown',
+        price: 60,
+        image: 'assets/lace-crown.JPG',
+        threestrand: true,
+        fivestrand: false
+    },
+    {
+        id: 6,
+        name: 'Seven Strand Crown',
+        price: 60,
+        image: 'assets/lace-crown.JPG',
+        threestrand: false,
+        fivestrand: false,
+        sevenstrand: true
+    },
+    {
+        id: 7,
+        name: 'Mermaid Braid',
+        price: 60,
+        image: 'assets/seven-strand-down',
+        threestrand: false,
+        fivestrand: false,
+        sevenstrand: true
+    }
+];
